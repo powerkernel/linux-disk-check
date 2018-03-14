75dd66da3cc33ae90233ebca263797dc0ed16e70
Harry Tang <harry@powerkernel.com>
2018-03-14 17:50:41 +0700

    upload script

---
+++ disk-check.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+MAX=90
+EMAIL=YOUR_EMAIL@DOMAIN.COM
+SERV=HOSTNAME.DOMAIN.COM
+
+# now check part 1
+O1=/dev/vda1
+USE=`df -h |grep ${O1} | awk '{ print $5 }' | cut -d'%' -f1`
+if [ ${USE} -gt ${MAX} ] ; then
+  echo "Percent used in $O1: $USE" | mail -s "$SERV is running out of disk space" ${EMAIL}
+  #echo "Percent used in $O1: $USE"
+fi
+
+# now check part 1
+#O2=/dev/vdb1
+#USE=`df -h |grep ${O2} | awk '{ print $5 }' | cut -d'%' -f1`
+#if [ ${USE} -gt ${MAX} ] ; then
+#  echo "Percent used in $O2: $USE" | mail -s "$SERV is running out of disk space" ${EMAIL}
+#  #echo "Percent used in $O2: $USE"
+#fi
