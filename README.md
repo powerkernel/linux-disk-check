e5a2906114ce1da1dcd1a44875b08f5d8f5f88d9
Harry Tang <harry@powerkernel.com>
2019-03-07 14:01:02 +0200

    update

--- README.md
+++ README.md
@@ -2,14 +2,4 @@
 A script that checks and send email notifications if your Linux server is running out of disk space
 
 ## Usage
-Edit `MAX`, `EMAIL`, `SERV`, `O1`, `O2`... in *disk-check.sh* and upload to your Linux server, then config crontab to run the script daily.
-
-If you need to check more partitions, just add more codes in the script like this:
-
-```sh
-O3=/dev/vdc1
-USE=`df -h |grep ${O3} | awk '{ print $5 }' | cut -d'%' -f1`
-if [ ${USE} -gt ${MAX} ] ; then
-  echo "Percent used in $O3: $USE" | mail -s "$SERV is running out of disk space" ${EMAIL}
-fi
-```
+Use with mailgun template
--- disk-check.sh
+++ disk-check.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
-MAX=90
-EMAIL=YOUR_EMAIL@DOMAIN.COM
-SERV=HOSTNAME.DOMAIN.COM
+MAX=75
+NAME=$(hostname -s)
+TO=YOUR_EMAIL@DOMAIN.COM
+SERV=$(hostname -f)
 
-# now check part 1
-O1=/dev/vda1
-USE=`df -h |grep ${O1} | awk '{ print $5 }' | cut -d'%' -f1`
+# now check disk 1
+D1=/dev/xvda1
+USE=`df -h |grep ${D1} | awk '{ print $5 }' | cut -d'%' -f1`
 if [ ${USE} -gt ${MAX} ] ; then
-  echo "Percent used in $O1: $USE" | mail -s "$SERV is running out of disk space" ${EMAIL}
+	curl -s --user 'api:YOUR_MAILGUN_API' \
+		 https://api.mailgun.net/v3/YOUR_DOMAIN/messages \
+		 -F from=''${NAME}' <root@'${SERV}'>' \
+		 -F to=''${TO}'' \
+		 -F subject="$SERV is running out of disk space" \
+		 -F template='disk-space-warning' \
+		 -F h:X-Mailgun-Variables='{"hostname": "'${SERV}'", "disk":"'${D1}'", "percent":"'${USE}'"}'
 fi
-
-# now check part 1
-#O2=/dev/vdb1
-#USE=`df -h |grep ${O2} | awk '{ print $5 }' | cut -d'%' -f1`
-#if [ ${USE} -gt ${MAX} ] ; then
-#  echo "Percent used in $O2: $USE" | mail -s "$SERV is running out of disk space" ${EMAIL}
-#fi
